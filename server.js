11920f89644a3616b097434dbad7854bd7d776b9
git <sygnowski.m@gmail.com>
2019-04-02 13:07:08 +0100

    heroku test v11

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
   //
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendfile(path.join((__dirname = "build/index.html")));
   });
 }
