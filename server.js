d05f3ff7b319a7ab4e31a43d80d591aa779ed876
git <sygnowski.m@gmail.com>
2019-04-29 22:33:25 +0100

    netifly test v7

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+"use strict";
 require("dotenv").config();
 
 const cheerio = require("cheerio");
@@ -22,9 +23,9 @@ const db = pgp({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "build")));
+// }
 
 app.post("/api/p4d", (req, res) => {
   // (async () => {
