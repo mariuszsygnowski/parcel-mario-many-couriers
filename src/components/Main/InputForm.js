4b4d5bd524154249d04d9eb5efa236b01e2b84a3
git <sygnowski.m@gmail.com>
2019-05-17 16:43:26 +0100

    set displayed values to empty value however default values in Store has not been changed until value from input will change

--- src/components/Main/InputForm.js
+++ src/components/Main/InputForm.js
@@ -4,6 +4,11 @@ const InputForm = props => {
   // useEffect(() => {
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // });
+  const [inputValue, setinputValue] = useState("");
+  const handleChange = e => {
+    props.setValue(e);
+    setinputValue(e);
+  };
   return (
     <div
     // className={props.nameClass}
@@ -12,8 +17,8 @@ const InputForm = props => {
         {props.labelName}
         <input
           type={props.inputType}
-          value={props.val}
-          onChange={event => props.setValue(event.target.value)}
+          value={inputValue}
+          onChange={event => handleChange(event.target.value)}
           placeholder={props.placeholder}
         />
       </label>
