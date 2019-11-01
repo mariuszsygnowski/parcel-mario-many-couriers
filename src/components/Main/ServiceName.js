93f98fb286f3d9b20fa6fab4dad2c8df55b814fb
git <sygnowski.m@gmail.com>
2019-11-01 19:15:47 +0000

    testing when clicking to url in service name

--- src/components/Main/ServiceName.js
+++ src/components/Main/ServiceName.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const ServiceName = props => {
   return (
     <div className={props.className}>
       <div className={`${props.className}__label`}>
         <p>
-          {props.res_result_data.service_name} (
-          {props.res_result_data.entries.length})
+          {props.res_result_data.service_name} ({props.res_result_data.entries.length})
         </p>
         <p>from: £{props.res_result_data.min_price_service_name.toFixed(2)}</p>
       </div>
       <div className={`${props.className}__buttons`}>
         {props.res_result_data.entries.map(res => {
           return (
-            <button
-              className={`${props.className}__buttons__buton`}
-              key={res.id}
-            >
-              {res.company_name} from: £{res.price.toFixed(2)}
+            <button className={`${props.className}__buttons__button`} key={res.id}>
+              <a id='myLink' href={res.company_name} target='_blank'>
+                {res.company_name} from: £{res.price.toFixed(2)}
+              </a>
             </button>
           );
         })}
